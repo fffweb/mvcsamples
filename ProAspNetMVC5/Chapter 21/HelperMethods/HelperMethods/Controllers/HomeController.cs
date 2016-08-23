b2345cf3ed9534423cab30462e9448c97c6925a5
fffweb <fffweb@hotmail.com>
2016-08-23 14:17:36 +0800

    my commit

--- ProAspNetMVC5/Chapter 21/HelperMethods/HelperMethods/Controllers/HomeController.cs
+++ ProAspNetMVC5/Chapter 21/HelperMethods/HelperMethods/Controllers/HomeController.cs
@@ -14,6 +14,28 @@ namespace HelperMethods.Controllers {
             return View((object)message);
         }
 
+        public ActionResult IndexInline()
+        {
+
+            ViewBag.Fruits = new string[] { "Apple", "Orange", "Pear" };
+            ViewBag.Cities = new string[] { "New York", "London", "Paris" };
+
+            string message = "This is an HTML element: <input>";
+
+            return View((object)message);   
+        }
+
+        public ActionResult IndexExternal()
+        {
+
+            ViewBag.Fruits = new string[] { "Apple", "Orange", "Pear" };
+            ViewBag.Cities = new string[] { "New York", "London", "Paris" };
+
+            string message = "This is an HTML element: <input>";
+
+            return View((object)message);
+        }
+
         public ActionResult CreatePerson() {
             return View(new Person());
         }
@@ -21,6 +43,30 @@ namespace HelperMethods.Controllers {
         [HttpPost]
         public ActionResult CreatePerson(Person person) {
             return View(person);
+        }
+
+        public ActionResult CreatePersonM()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public ActionResult CreatePersonM(Person person)
+        {
+            return View(person);
+
+        }
+
+        public ActionResult CreatePersonH()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public ActionResult CreatePersonH(Person person)
+        {
+            return View(person);
+
         }
     }
 }
